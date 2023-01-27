acdd205976a8e8b0189b7aed1fc71004bd0db949
d.fenwick <d.fenwick@stpauls.qld.edu.au>
2023-01-27 12:44:57 +1000

    .

--- Unit1_Create_With_Code/mypython01.py
+++ Unit1_Create_With_Code/mypython01.py
@@ -5,11 +5,30 @@ line comment
 """
 # Input - asking user to input something and saving to a variable
 name = input("What is your name? ")
-age = input("What is your age? ")
-
+age = int(input("What is your age? "))
+print(type(name))
+print(type(age))
 # Processing
 # Python can calculate numbers
-age = 20 + age
+future = 20
+age = age + future
 
 # Output
 print("Hi, " +name, "Your age in 20 years will be", age)
+print(f"Hi {name}, your age in {future} years will be {age}.")  # preferred method using f-string
+
+# Our Technology context for Digital Solutions is:
+# "WEB APPLICATIONS" - using Python with web frameworks (Flask, HTML, CSS (Bootstrap)
+# To begin we need to understand and apply Python programming syntax (Python language and rules)
+# And we need to understand how to use the IDE - Pycharm
+
+# ACTION 1 - Can you Commit this 'mypython01.py' file to your GitHub account?
+
+# Pycharm Basics - Interpreter, Project Files, Running code, Terminal, Console, Packages, Tabs, Questions?
+
+# Python - input, process, output
+# Comments - # single line """ multiline
+# Variables -
+# Data Types - str, int, float, bool, list, tuple, dict
+# Built in functions
+# string to int = int("5")
