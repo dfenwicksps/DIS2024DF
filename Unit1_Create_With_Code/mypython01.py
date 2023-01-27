e250e70c0661c711b256cb9715dd5be11bd8652a
dfenwicksps <d.fenwick@stpauls.qld.edu.au>
2023-01-27 12:30:51 +1000

    Create mypython01.py

---
+++ Unit1_Create_With_Code/mypython01.py
@@ -0,0 +1,15 @@
+# This is a comment
+"""
+This is a multi
+line comment
+"""
+# Input - asking user to input something and saving to a variable
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+# Processing
+# Python can calculate numbers
+age = 20 + age
+
+# Output
+print("Hi, " +name, "Your age in 20 years will be", age)
